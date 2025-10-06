dfda776c9d214b6c935e18f769cd80be3082f40f
VKIM <vadim.siebelcrm@gmail.com>
2025-10-06 22:20:01 +0500

    fix error

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { ReactDOM } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import reportWebVitals from './reportWebVitals.ts'
 
-const root = createRoot(document.getElementById('root') as HTMLElement);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
   <React.StrictMode>
